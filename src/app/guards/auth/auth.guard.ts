685701a63b08b96c7a611688055f44bdb81f57a1
Bruno Joao <1353905+vinagreti@users.noreply.github.com>
2024-05-12 13:19:02 -0300

    remove logs

--- src/app/guards/auth/auth.guard.ts
+++ src/app/guards/auth/auth.guard.ts
@@ -6,10 +6,8 @@ import { map } from 'rxjs';
 export const authGuard: CanActivateFn = (_, { url: redirectUrl }) => {
   const router = inject(Router);
   const supabaseService = inject(SupabaseService);
-  console.log(redirectUrl);
   return supabaseService.session$.pipe(
     map((session) => {
-      console.log('session', session?.user?.user_metadata?.['name']);
       return (
         !!session?.user ||
         router.createUrlTree(['/auth'], {
